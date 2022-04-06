2323bccdf668430f1bbc8d94f202c67f6dc02fb3
zeus-magnum <llwina8@gmail.com>
2022-04-06 09:33:22 +0530

    update script & style

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ var col_02 = document.getElementById("rect_02");
 var col_03 = document.getElementById("rect_03");
 var col_04 = document.getElementById("rect_04");
 
-if(screen_width > 500){
+if(screen_width >= 500){
     col_01.style.width = "10%";
     col_02.style.width = "15%";
     col_03.style.width = "10%";
     col_04.style.width = "15%";
-    col_01.style.height = "100%";
-    col_02.style.height = "100%";
-    col_03.style.height = "100%";
-    col_04.style.height = "100%";
+    col_01.style.height = "150%";
+    col_02.style.height = "150%";
+    col_03.style.height = "150%";
+    col_04.style.height = "150%";
 }else{
     col_01.style.width = "5%";
     col_02.style.width = "8%";
     col_03.style.width = "5%";
     col_04.style.width = "8%";
-    col_01.style.height = "100%";
-    col_02.style.height = "100%";
-    col_03.style.height = "100%";
-    col_04.style.height = "100%";
+    col_01.style.height = "150%";
+    col_02.style.height = "150%";
+    col_03.style.height = "150%";
+    col_04.style.height = "150%";
 }
